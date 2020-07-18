08d82d669352f8afb127ffb8e1c2853906b4e9da
CacoCavalcanti <CacoCavalcanti@users.noreply.github.com>
2020-07-18 20:11:04 -0300

    Lets get started

---
+++ checklistExercice/checklistExercice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  checklistExercice
+//
+//  Created by Carlos Cavalcanti on 18/07/2020.
+//  Copyright © 2020 Carlos Cavalcanti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
